068e7d2ca3bb63bad2c53cc7f3c070de956120be
dhruvbaliyan <dhruvbaliyan1001@gmail.com>
2022-05-16 05:24:28 +0530

    Added ticket model

---
+++ src/models/ticket.rs
@@ -0,0 +1,39 @@
+use crate::models::{Comment, Title};
+use serde::{Deserialize, Serialize};
+use std::fmt::Error;
+use std::fmt::Formatter;
+
+pub type TicketId = u64;
+
+#[derive(PartialEq, Debug, Clone, Hash, Eq)]
+pub struct Ticket {
+    pub id: TicketId,
+    pub title: Title,
+    pub description: String,
+    pub status: Status,
+    pub comments: Vec<Comment>,
+}
+
+impl std::fmt::Display for Ticket {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        writeln!(
+            f,
+            "Ticket:\n\tId:{:?}\n\tTitle:{}\n\tDescription:{}\n\tStatus:{:?}\n\tComments:",
+            self.id, self.title, self.description, self.status
+        )?;
+        for comment in self.comments.iter() {
+            writeln!(f, "\t- {}", comment)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(PartialEq, Debug, Copy, Clone, Hash, Eq, Serialize, Deserialize)]
+pub enum Status {
+    ToDo,
+    InProgress,
+    Blocked,
+    Done,
+}
+
+pub struct DeletedTicket(pub Ticket);
